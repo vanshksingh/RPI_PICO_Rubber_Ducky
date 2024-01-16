4f12d26386fb7611c9ebb44d2b06e4312d3f1fc4
Vansh Kumar Singh <114809624+vanshksingh@users.noreply.github.com>
2024-01-17 04:03:06 +0530

    Update Terminal_html.ino

--- Terminal_html.ino
+++ Terminal_html.ino
@@ -53,16 +53,74 @@ void triggerAutomation() {
   delay(500);
 
   // Press Command + n
-Keyboard.press(KEY_LEFT_GUI);  // Press the Command (GUI) key
-Keyboard.press('n');           // Press the 'o' key
-delay(100);                    // Short delay to ensure the keystroke is registered
-Keyboard.releaseAll();         // Release all keys
+  Keyboard.press(KEY_LEFT_GUI);  // Press the Command (GUI) key
+  Keyboard.press('n');           // Press the 'o' key
+  delay(100);                    // Short delay to ensure the keystroke is registered
+  Keyboard.releaseAll();         // Release all keys
 
-// Wait for Terminal to open
+  // Wait for Terminal to open
   delay(800);
 
   // Command to create HTML file
-  Keyboard.print("echo '<!DOCTYPE html>\n<html>\n<head>\n<title>Rubber Ducky</title>\n</head>\n<body>\n<h1>This is too easy!</h1>\n</body>\n</html>' > ~/Documents/");
+  Keyboard.print("echo '<!DOCTYPE html>\\n");
+  Keyboard.print("<html>\\n");
+  Keyboard.print("<head>\\n");
+  Keyboard.print("<title>Page Title</title>\\n");
+  Keyboard.print("<style>\\n");
+  Keyboard.print("body {\\n");
+  Keyboard.print("display: flex;\\n");
+  Keyboard.print("align-items: center;\\n");
+  Keyboard.print("justify-content: center;\\n");
+  Keyboard.print("font-family: Arial, sans-serif;\\n");
+  Keyboard.print("line-height: 1.8;\\n");
+  Keyboard.print("min-height: 100vh;\\n");
+  Keyboard.print("background: #ffffff;\\n");
+  Keyboard.print("flex-direction: column;\\n");
+  Keyboard.print("}\\n");
+  Keyboard.print(".main {\\n");
+  Keyboard.print("background-color: #fff;\\n");
+  Keyboard.print("border-radius: 15px;\\n");
+  Keyboard.print("box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);\\n");
+  Keyboard.print("padding: 20px;\\n");
+  Keyboard.print("transition: transform 0.2s;\\n");
+  Keyboard.print("width: 600px;\\n");
+  Keyboard.print("}\\n");
+  Keyboard.print("form {\\n");
+  Keyboard.print("display: flex;\\n");
+  Keyboard.print("align-items: center;\\n");
+  Keyboard.print("justify-content: center;\\n");
+  Keyboard.print("flex-direction: column;\\n");
+  Keyboard.print("}\\n");
+  Keyboard.print("h1 {\\n");
+  Keyboard.print("text-align: center;\\n");
+  Keyboard.print("color: red;\\n");
+  Keyboard.print("}\\n");
+  Keyboard.print("input {\\n");
+  Keyboard.print("padding: 10px;\\n");
+  Keyboard.print("}\\n");
+  Keyboard.print("</style>\\n");
+  Keyboard.print("</head>\\n");
+  Keyboard.print("<body>\\n");
+  Keyboard.print("<div class='main'>\\n");
+  Keyboard.print("<h1>Welcome To Rubber Ducky</h1>\\n");
+  Keyboard.print("<form>\\n");
+  Keyboard.print("<p>\\n");
+  Keyboard.print("<label>Username :\\n");
+  Keyboard.print("<input type='text' /></label>\\n");
+  Keyboard.print("</p>\\n");
+  Keyboard.print("<p>\\n");
+  Keyboard.print("<label>Password :\\n");
+  Keyboard.print("<input type='password' /></label>\n");
+  Keyboard.print("</p>\n");
+  Keyboard.print("<p>\n");
+  Keyboard.print("<button type='submit'>Submit</button>\n");
+  Keyboard.print("</p>\n");
+  Keyboard.print("</form>\n");
+  Keyboard.print("</div>\n");
+  Keyboard.print("</body>\n");
+  Keyboard.print("</html>' > ~/Documents/");
+
+
   Keyboard.println(randomString);
   delay(500);
 
@@ -82,6 +140,4 @@ Keyboard.releaseAll();         // Release all keys
   delay(100);
   Keyboard.releaseAll();
   delay(1000);
-
- 
 }
